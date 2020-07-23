3aa0c29d6725ff1242146e56f40bb424f597fe8a
Aryaman19 <aryaman.thakur999888@gmail.com>
2020-07-23 16:12:52 +0530

    Production

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+
+require("./models/Users");
+require("./services/passport");
 
 const app = express();
 
-app.get("/", (req, res) => {
-	res.send({ hi: "there" });
-});
+app.use(
+    cookieSession({
+             maxAge= 30*24*60*60*1000,
+             keys:[keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect(keys.mongoURI);
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 
