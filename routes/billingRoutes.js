8e5e8427390879135a0ce7b96d504d3e0916b092
Aryaman19 <aryaman.thakur999888@gmail.com>
2020-08-03 00:23:01 +0530

    Final Commit

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -6,8 +6,8 @@ module.exports = (app) => {
 	app.post("/api/stripe", requireLogin, async (req, res) => {
 		const charge = await stripe.charges.create({
 			amount: 500,
-			currency: "usd",
-			description: "$5 for 5 credits",
+			currency: "inr",
+			description: "50Rs for 5 credits",
 			source: req.body.id,
 		});
 
